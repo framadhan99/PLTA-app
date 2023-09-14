c21822dc3d5417e6a96595803ff804110ada026e
framadhan99 <fajarramadhan99704@gmail.com>
2023-09-14 16:31:32 +0700

    text extend

--- lib/pages/detail_laporan_page.dart
+++ lib/pages/detail_laporan_page.dart
@@ -6,9 +6,15 @@ import 'package:plta_app/config/ui_helper.dart';
 import 'package:plta_app/widgets/card/card_grey.dart';
 import 'package:plta_app/widgets/card/card_shadow_widget.dart';
 
-class DetailLaporanPage extends StatelessWidget {
+class DetailLaporanPage extends StatefulWidget {
   const DetailLaporanPage({super.key});
 
+  @override
+  State<DetailLaporanPage> createState() => _DetailLaporanPageState();
+}
+
+class _DetailLaporanPageState extends State<DetailLaporanPage> {
+  bool more = false;
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -69,7 +75,7 @@ class DetailLaporanPage extends StatelessWidget {
               (index) => Padding(
                 padding: EdgeInsets.only(bottom: 16),
                 child: CardShadow(
-                  height: 125,
+                  height: more ? 125 : screenHeightPercentage(context) * 0.6,
                   width: screenWidth(context),
                   child: Column(
                     crossAxisAlignment: CrossAxisAlignment.start,
@@ -106,13 +112,160 @@ class DetailLaporanPage extends StatelessWidget {
                         ),
                       ),
                       verticalSpace(12),
-                      Text(
-                        'Lihat selengkapnya',
-                        style: AssetStyle.desText.copyWith(
-                          fontWeight: FontWeight.w500,
-                          color: AssetColors.blue,
-                        ),
-                      ),
+                      more
+                          ? GestureDetector(
+                              onTap: () {
+                                setState(() {
+                                  more = !more;
+                                });
+                              },
+                              child: Text(
+                                'Lihat selengkapnya',
+                                style: AssetStyle.desText.copyWith(
+                                  fontWeight: FontWeight.w500,
+                                  color: AssetColors.blue,
+                                ),
+                              ),
+                            )
+                          : Column(
+                              crossAxisAlignment: CrossAxisAlignment.start,
+                              children: [
+                                CardGrey(
+                                  height:
+                                      screenHeightPercentage(context) * 0.43,
+                                  child: Column(
+                                    crossAxisAlignment:
+                                        CrossAxisAlignment.start,
+                                    children: [
+                                      Text(
+                                        'Kondisi',
+                                        style: AssetStyle.desText.copyWith(
+                                          fontSize: 12,
+                                        ),
+                                      ),
+                                      verticalSpace(4),
+                                      Text(
+                                        'Baik',
+                                      ),
+                                      verticalSpace(8),
+                                      Divider(),
+                                      verticalSpace(8),
+                                      Text(
+                                        'Judul Pengecekan',
+                                        style: AssetStyle.desText.copyWith(
+                                          fontSize: 12,
+                                        ),
+                                      ),
+                                      verticalSpace(4),
+                                      Text(
+                                        'Expansion join dicek secara rutin',
+                                      ),
+                                      verticalSpace(8),
+                                      Divider(),
+                                      verticalSpace(8),
+                                      Text(
+                                        'Keterangan Pengecekan',
+                                        style: AssetStyle.desText.copyWith(
+                                          fontSize: 12,
+                                        ),
+                                      ),
+                                      verticalSpace(4),
+                                      Text(
+                                        'Berjalan cukup baik, tidak perlu perawatan lebih',
+                                      ),
+                                      verticalSpace(8),
+                                      Divider(),
+                                      verticalSpace(8),
+                                      Text(
+                                        'Solusi Pengecekan',
+                                        style: AssetStyle.desText.copyWith(
+                                          fontSize: 12,
+                                        ),
+                                      ),
+                                      verticalSpace(4),
+                                      Text(
+                                        'Tidak perlu pengecekan berlanjut',
+                                      ),
+                                      verticalSpace(8),
+                                      Divider(),
+                                      verticalSpace(8),
+                                      Text(
+                                        'Bukti Foto Pengecekan',
+                                        style: AssetStyle.desText.copyWith(
+                                          fontSize: 12,
+                                        ),
+                                      ),
+                                      verticalSpace(8),
+                                      Row(
+                                        crossAxisAlignment:
+                                            CrossAxisAlignment.center,
+                                        children: [
+                                          Container(
+                                            width: 45,
+                                            height: 45,
+                                            decoration: BoxDecoration(
+                                              borderRadius:
+                                                  BorderRadius.circular(4),
+                                              color: Colors.grey,
+                                              image: DecorationImage(
+                                                fit: BoxFit.cover,
+                                                image: AssetImage(
+                                                  AssetPaths.imgMesin,
+                                                ),
+                                              ),
+                                            ),
+                                          ),
+                                          horizontalSpace(12),
+                                          Column(
+                                            mainAxisAlignment:
+                                                MainAxisAlignment.center,
+                                            crossAxisAlignment:
+                                                CrossAxisAlignment.start,
+                                            children: [
+                                              Text(
+                                                'Mesin T1',
+                                                style: AssetStyle.primaryText
+                                                    .copyWith(
+                                                  fontWeight: FontWeight.w700,
+                                                  color:
+                                                      AssetColors.primaryColor,
+                                                ),
+                                              ),
+                                              verticalSpace(4),
+                                              Text(
+                                                'EGN-OT-001001',
+                                                style:
+                                                    AssetStyle.desText.copyWith(
+                                                  fontWeight: FontWeight.w500,
+                                                  fontSize: 10,
+                                                  color:
+                                                      AssetColors.primaryColor,
+                                                ),
+                                              ),
+                                            ],
+                                          ),
+                                        ],
+                                      ),
+                                    ],
+                                  ),
+                                ),
+                                verticalSpace(16),
+                                GestureDetector(
+                                  onTap: () {
+                                    setState(() {
+                                      more = !more;
+                                    });
+                                  },
+                                  child: Text(
+                                    'Sembunyikan',
+                                    style: AssetStyle.desText.copyWith(
+                                      fontWeight: FontWeight.w500,
+                                      color: AssetColors.blue,
+                                    ),
+                                  ),
+                                ),
+                              ],
+                            ),
                     ],
                   ),
                 ),
