5da73b5054e622c4611c897b759ecf3e036df0de
framadhan99 <fajarramadhan99704@gmail.com>
2023-09-20 09:04:11 +0700

    show dialog

--- lib/pages/permintaan_perbaikan_page.dart
+++ lib/pages/permintaan_perbaikan_page.dart
@@ -8,7 +8,9 @@ import 'package:plta_app/config/ui_helper.dart';
 import 'package:plta_app/widgets/appbar/appbar_primary.dart';
 import 'package:plta_app/widgets/button/button_primary.dart';
 import 'package:plta_app/widgets/button/button_radio.dart';
+import 'package:plta_app/widgets/card/card_grey.dart';
 import 'package:plta_app/widgets/texfield/textfield_general_widget.dart';
+import 'package:plta_app/widgets/texfield/textfield_search.dart';
 
 class PermintaanPerbaikanPage extends StatefulWidget {
   const PermintaanPerbaikanPage({super.key});
@@ -204,33 +206,117 @@ class _PermintaanPerbaikanPageState extends State<PermintaanPerbaikanPage> {
               ),
             ),
             verticalSpace(16),
-            Container(
-              padding: EdgeInsets.symmetric(horizontal: 16),
-              width: screenWidth(context),
-              height: 40,
-              decoration: BoxDecoration(
-                color: AssetColors.greyLightes,
-                border: Border.all(
-                  color: AssetColors.greyLight,
-                  style: BorderStyle.none,
-                ),
-                borderRadius: BorderRadius.circular(4),
-              ),
-              child: Row(
-                children: [
-                  Icon(
-                    Icons.add,
-                    size: 20,
+            GestureDetector(
+              onTap: () {
+                showDialog(
+                  context: context,
+                  builder: (context) {
+                    return Dialog(
+                      insetPadding: const EdgeInsets.symmetric(
+                          horizontal: 20, vertical: 16),
+                      shape: RoundedRectangleBorder(
+                          borderRadius: BorderRadius.circular(16)),
+                      child: Container(
+                        padding: EdgeInsets.all(16),
+                        height: screenHeightPercentage(context) * 0.8,
+                        width: screenWidth(context),
+                        child: Column(
+                          children: [
+                            Align(
+                              alignment: Alignment.topLeft,
+                              child: GestureDetector(
+                                onTap: () {
+                                  Navigator.pop(context);
+                                },
+                                child: Icon(
+                                  Icons.close,
+                                ),
+                              ),
+                            ),
+                            verticalSpace(16),
+                            const Text(
+                              'Pilih Komponen / Sub Komponen',
+                              style: AssetStyle.bigTitle,
+                            ),
+                            verticalSpace(16),
+                            TextFieldSearch(),
+                            verticalSpace(16),
+                            Container(
+                              height: screenHeightPercentage(context) * 0.59,
+                              child: SingleChildScrollView(
+                                child: Column(
+                                  children: [
+                                    ...List.generate(
+                                      10,
+                                      (int index) => Padding(
+                                        padding:
+                                            const EdgeInsets.only(bottom: 16),
+                                        child: CardGrey(
+                                            padding: const EdgeInsets.symmetric(
+                                              horizontal: 16,
+                                              vertical: 16,
+                                            ),
+                                            height: 66,
+                                            child: Column(
+                                              crossAxisAlignment:
+                                                  CrossAxisAlignment.start,
+                                              children: [
+                                                Text(
+                                                  'Draft Tube Manholes',
+                                                  style: AssetStyle.primaryText
+                                                      .copyWith(
+                                                          fontSize: 12,
+                                                          fontWeight:
+                                                              FontWeight.w600),
+                                                ),
+                                                verticalSpace(4),
+                                                Text(
+                                                  'KMP-002001',
+                                                  style: AssetStyle.desText,
+                                                ),
+                                              ],
+                                            )),
+                                      ),
+                                    ),
+                                  ],
+                                ),
+                              ),
+                            )
+                          ],
+                        ),
+                      ),
+                    );
+                  },
+                );
+              },
+              child: Container(
+                padding: EdgeInsets.symmetric(horizontal: 16),
+                width: screenWidth(context),
+                height: 40,
+                decoration: BoxDecoration(
+                  color: AssetColors.greyLightes,
+                  border: Border.all(
                     color: AssetColors.greyLight,
+                    style: BorderStyle.none,
                   ),
-                  horizontalSpace(8),
-                  Text(
-                    'Tambah Komponen / Sub Komponen',
-                    style: AssetStyle.primaryText.copyWith(
+                  borderRadius: BorderRadius.circular(4),
+                ),
+                child: Row(
+                  children: [
+                    Icon(
+                      Icons.add,
+                      size: 20,
                       color: AssetColors.greyLight,
                     ),
-                  ),
-                ],
+                    horizontalSpace(8),
+                    Text(
+                      'Tambah Komponen / Sub Komponen',
+                      style: AssetStyle.primaryText.copyWith(
+                        color: AssetColors.greyLight,
+                      ),
+                    ),
+                  ],
+                ),
               ),
             ),
           ],
